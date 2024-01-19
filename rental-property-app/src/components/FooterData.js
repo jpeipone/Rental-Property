0e6024af228f7466e04a4acf999ee05c52ce75c7
Janne Peiponen <janne.peiponen@edu.karelia.fi>
2024-01-19 12:12:10 +0200

    firebase hosting

--- rental-property-app/src/components/FooterData.js
+++ rental-property-app/src/components/FooterData.js
@@ -5,6 +5,11 @@ const FooterData = () => {
   return (
     <div className="footer-container">
       <div className="footer__header">AsuntoX</div>
+      <p>Asuntosijoittajan työkalu</p>
+      <p>
+        Laskurin arvot ovat suuntaa antavia ja voi sisältää virheitä, eikä niitä
+        pidä käyttää sijotuspäätöksissä
+      </p>
     </div>
   );
 };
